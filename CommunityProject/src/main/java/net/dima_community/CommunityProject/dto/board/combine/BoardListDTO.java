c3fd8e2cbb00975350959b71f45a69323c26fbda
Yeonnnny <kdoy030@gmail.com>
2024-09-01 18:54:02 +0900

    240901 BoardListDTO에 Dday 변수 추가 및 계산 함수 로직 추가

--- CommunityProject/src/main/java/net/dima_community/CommunityProject/dto/board/combine/BoardListDTO.java
+++ CommunityProject/src/main/java/net/dima_community/CommunityProject/dto/board/combine/BoardListDTO.java
@@ -1,6 +1,7 @@
 package net.dima_community.CommunityProject.dto.board.combine;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -26,7 +27,36 @@ public class BoardListDTO { // 게시물 목록 화면에 필요한 속성들만
     private LocalDateTime createDate;
     // activity / recruit 
     private LocalDateTime deadline;
+    private int dDay;
     private int limitNumber;
     private int currentNumber;
+
+    public BoardListDTO(Long boardId, String memberId, String memberGroup, String title, int hitCount, int likeCount, int replyCount, LocalDateTime createDate, LocalDateTime deadline, int limitNumber, int currentNumber) {
+        this.boardId = boardId;
+        this.memberId = memberId;
+        this.memberGroup = memberGroup;
+        this.title = title;
+        this.hitCount = hitCount;
+        this.likeCount = likeCount;
+        this.replyCount = replyCount;
+        this.createDate = createDate;
+        this.deadline = deadline;
+        this.limitNumber = limitNumber;
+        this.currentNumber = currentNumber;
+
+        updateDDay(); // dDay 값을 계산하여 설정
+    }
+
+    /**
+     * D-Day 계산 함수
+     */
+    private void updateDDay() {
+        if (this.deadline != null) {
+            LocalDateTime now = LocalDateTime.now();
+            this.dDay = (int) ChronoUnit.DAYS.between(now.toLocalDate(), this.deadline.toLocalDate());
+        } else {
+            this.dDay = 0; // 또는 다른 기본값
+        }
+    }
 
 }
